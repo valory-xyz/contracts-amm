ef5711abe7f7b7b9b3b4de25260dee7947555661
David Vilela <dvilelaf@gmail.com>
2021-11-25 17:19:08 +0100

    fix: get correct contract for funds stage

--- deploy/4_funds.js
+++ deploy/4_funds.js
@@ -1,9 +1,11 @@
 module.exports = async (hre) => {
     // Send tokens to traders
     const accounts = ["0xC8b5Fa6F45d42FeD46156B5CbD543edD0d8D18aB",
-                    "0xA8705818909Cc6E5748a703857C4C9Fd37f881ac",
-                    "0x99C49aF33d27D64A95E6F8F190c4FBc23dBEFAF7",
-                    "0x38C1A04b64D653352edef30Df28F8871a93Eb1E5"];
+                      "0xA8705818909Cc6E5748a703857C4C9Fd37f881ac",
+                      "0x99C49aF33d27D64A95E6F8F190c4FBc23dBEFAF7",
+                      "0x38C1A04b64D653352edef30Df28F8871a93Eb1E5"];
+
+    tokenA = await hre.ethers.getContractAt("ERC20PresetFixedSupply", "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9");
 
     owner_balance = await tokenA.balanceOf(tokenA_owner);
     console.log("Token A owner balance: ", owner_balance.toString());
