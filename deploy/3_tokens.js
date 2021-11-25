efdcdd2ae6de701d5f564d194ddc5a79057f40bb
David Vilela <dvilelaf@gmail.com>
2021-11-25 19:25:19 +0100

    chore: use different supplies for both tokens

--- deploy/3_tokens.js
+++ deploy/3_tokens.js
@@ -7,11 +7,12 @@ module.exports = async (hre) => {
     tokenA_owner = accounts[0].address;
     tokenB_owner = accounts[1].address;
 
-    const INITIAL_SUPPLY = 1000000;
+    const INITIAL_SUPPLY_A = 1000000;
+    const INITIAL_SUPPLY_B = 100000;
 
     const Token = await hre.ethers.getContractFactory("ERC20PresetFixedSupply");
-    const tokenA = await Token.deploy("TokenA", "TOKA", INITIAL_SUPPLY, tokenA_owner);
-    const tokenB = await Token.deploy("TokenB", "TOKB", INITIAL_SUPPLY, tokenB_owner);
+    const tokenA = await Token.deploy("TokenA", "TOKA", INITIAL_SUPPLY_A, tokenA_owner);
+    const tokenB = await Token.deploy("TokenB", "TOKB", INITIAL_SUPPLY_B, tokenB_owner);
     await tokenA.deployed();
     await tokenB.deployed();
     console.log("Token A deployed to:", tokenA.address);
