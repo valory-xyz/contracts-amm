31a7893faae0c8e39323c8f0949648a8a7c54a3e
David Vilela <dvilelaf@gmail.com>
2021-11-25 19:22:40 +0100

    chore: do not use random feeToSetter address

--- deploy/2_uni.js
+++ deploy/2_uni.js
@@ -2,16 +2,17 @@ module.exports = async (hre) => {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
   const {deployer} = await getNamedAccounts();
+  const accounts = await hre.ethers.getSigners();
 
   // Deploy Uniswap factory
-  feeToSetter_address = "0xc0ffee254729296a45a3885639AC7E10F9d54979"; // random for now
+  feeToSetter_address = accounts[2].address;
   const Factory = await hre.ethers.getContractFactory("UniswapV2Factory");
   const factory = await Factory.deploy(feeToSetter_address);
 
   await factory.deployed();
 
   // Deploy Router02
-  weth_address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  weth_address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
   const Router = await hre.ethers.getContractFactory("UniswapV2Router02");
   const router = await Router.deploy(factory.address, weth_address);
 
