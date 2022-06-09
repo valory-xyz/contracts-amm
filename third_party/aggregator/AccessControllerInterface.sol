05d00b238c1de07de26a3378ca79e7577dd271ae
David Vilela <dvilelaf@gmail.com>
2022-06-09 09:28:49 +0200

    fix: delete unneeded contracts

--- third_party/aggregator/AccessControllerInterface.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-// https://github.com/smartcontractkit/libocr/blob/d12971936c1289ae0d512723a9e8535ce382ff6d/contract/AccessControllerInterface.sol
-pragma solidity ^0.7.0;
-
-interface AccessControllerInterface {
-  function hasAccess(address user, bytes calldata data) external view returns (bool);
-}
